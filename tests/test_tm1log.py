8d693e63815e16528bb309d0a21329da3d0a2c39
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2022-12-23 21:55:12 +0100

    feat: trivial test

--- tests/test_tm1log.py
+++ tests/test_tm1log.py
@@ -1,4 +1,24 @@
+import csv
+
+from plugins.vd_tm1log import TM1LogRow, remove_metadata_lines
+
 # import pytest
 
 
 sample_file = "tests/sample.log"
+
+
+def test_remove_metadata_lines():
+
+    with open(sample_file) as fp:
+
+        reader = csv.reader(remove_metadata_lines(fp))
+
+        row = next(reader)
+
+        assert row
+
+        tm1log_row = TM1LogRow(row)
+
+        assert tm1log_row.user == "Admin"
+        assert tm1log_row.type == "S"
