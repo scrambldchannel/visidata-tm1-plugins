9f2a933900c84ce9b43cec5eb93edb80b5c1ed55
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2022-12-27 15:41:39 +0100

    feat: test all fixed cols

--- tests/test_tm1log.py
+++ tests/test_tm1log.py
@@ -25,6 +25,20 @@ def test_cols(sample_log_sheet):
     assert sample_log_sheet.columns[0].name == "Time"
     assert sample_log_sheet.columns[1].name == "Cube"
 
+    assert sample_log_sheet.columns[2].name == "User"
+    assert sample_log_sheet.columns[3].name == "T"
+    assert sample_log_sheet.columns[4].name == "El Cnt"
+    assert sample_log_sheet.columns[5].name == "El 1"
+    assert sample_log_sheet.columns[6].name == "El 2"
+
+    assert sample_log_sheet.columns[-6].name == "Old Val N"
+    assert sample_log_sheet.columns[-5].name == "New Val N"
+    assert sample_log_sheet.columns[-4].name == "Delta"
+    assert sample_log_sheet.columns[-3].name == "Abs Delta"
+
+    assert sample_log_sheet.columns[-2].name == "Old Val S"
+    assert sample_log_sheet.columns[-1].name == "New Val S"
+
 
 def test_rows(sample_log_sheet):
 
